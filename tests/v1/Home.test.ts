f97021f73dc0819589c7f07ba87c9341dd9047db
leanhdung278 <leanhdungxxxx@gmail.com>
2020-12-03 09:22:57 +0700

    Update testing

--- tests/v1/Home.test.ts
+++ tests/v1/Home.test.ts
@@ -6,11 +6,10 @@ import Locals from '../../src/providers/Locals';
 chai.use(chaiHttp);
 
 let should = chai.should();
+let expect = chai.expect;
 let server = `http://localhost:${Locals.config().port}/${
     Locals.config().apiPrefixV1
 }`;
-
-console.log(server, 'SERVER TEST');
 
 /*
  * Test the /GET route
@@ -20,7 +19,8 @@ describe('/GET Home Page', () => {
         chai.request(server)
             .get('/')
             .end((err, res) => {
-                res.should.have.status(200);
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
                 done();
             });
     });
